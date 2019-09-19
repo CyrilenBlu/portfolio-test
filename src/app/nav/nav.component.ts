39aadf4b37e09245b11368c47a1a659259327a47
CyrilenBlu <lukepetzerint2@gmail.com>
2019-09-19 17:44:52 +0200

    Added custom CSS and script, customized skills, todo: add more skills, optimize css.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
-  templateUrl: 'nav.html'
+  templateUrl: 'nav.component.html',
+  styleUrls: [ 'nav.component.css' ]
 })
 
-export class NavComponent {}
+export class NavComponent implements OnInit{
+
+  constructor(private router : Router) {
+
+  }
+
+  ngOnInit(): void {
+    let location = this.router.url;
+    location = location.charAt(1).toUpperCase() +  location.substring(2, location.length);
+    console.log(location);
+    //Previous Work handle
+    if (location == "Previouswork") {
+      location = location.substring(0, location.indexOf("s") + 1) + " " +
+        location.charAt(location.indexOf("w")).toUpperCase() +
+        location.substring(location.indexOf("s") + 2, location.length);
+    }
+    if (location == "") {
+      location = "Luke Petzer";
+    }
+    document.getElementById("title").innerText = location;
+  }
+}
